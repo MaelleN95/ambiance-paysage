45370750d137a7f4cc8262f81097b0b59811c5fc
Maëlle Nioche <maelle.nioche@gmail.com>
2025-12-15 15:31:35 +0100

    feat(legal-notice): created the legal notices and privacy policy page

--- ambiance-paysage-app/src/Controller/LegalNoticeController.php
+++ ambiance-paysage-app/src/Controller/LegalNoticeController.php
@@ -6,13 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class LegalController extends AbstractController
+final class LegalNoticeController extends AbstractController
 {
-    #[Route('/mentions-legales', name: 'app_legal')]
+    #[Route('/mentions-legales', name: 'app_legal_notice')]
     public function index(): Response
     {
-        return $this->render('legal/index.html.twig', [
-            'controller_name' => 'LegalController',
-        ]);
+        return $this->render('legal_notice/index.html.twig');
     }
 }
